ce1c96d0124c0b62dcb697f2b39456827282be82
sandrine <liaworkingonline@gmail.com>
2021-03-03 14:33:38 +0100

    first commit

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="hello">
+    HomePage
+    <button src="#"><router-link to="/results" >Go to resultsPage</router-link></button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+    };
+  },
+};
+</script>
---
+++ src/pages/ResultsPage.vue
@@ -0,0 +1,20 @@
+<script src="https://unpkg.com/vue/dist/vue.js"></script>
+<script src="https://unpkg.com/vue-router/dist/vue-router.js"></script>
+
+<template>
+  <div class="hello">
+    ResultsPage
+    <button src="#"><router-link to="/" >Go back to HomePage</router-link></button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+    };
+  },
+};
+</script>
